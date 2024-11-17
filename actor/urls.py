aecffc6399204987a724d5c17c1ba18f129ab881
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2024-11-17 12:37:41 +0100

    file renaming

--- actor/urls.py
+++ actor/urls.py
@@ -1,6 +1,6 @@
 # myapp/urls.py
 from django.urls import path
-from .views import UserRegistrationView
+from .views.registration import UserRegistrationView
 
 urlpatterns = [
     path('auth/', UserRegistrationView.as_view(), name='user-registration'),
--- actor/views/registration.py
+++ actor/views/registration.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 
-from .models import Wallet
-from .serializers import UserRegistrationSerializer
+from actor.models import Wallet
+from actor.serializers import UserRegistrationSerializer
 from services.otp import OTPService  # Le service OTP importé ici
 
 class UserRegistrationView(APIView):
