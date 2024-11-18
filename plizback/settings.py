5449f44cd6ab522ca9eb1fb7f85ba111e5b7685e
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2024-11-18 15:31:40 +0100

    feat : add swagger

--- plizback/settings.py
+++ plizback/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
 
     'actor',
     'transaction'
@@ -124,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- plizback/urls.py
+++ plizback/urls.py
@@ -17,12 +17,46 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Définir les informations de base de l'API
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Plizback API",
+        default_version='v1',
+        description="API de gestion de transactions",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="williams.stanley.desouza@octo.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
+
+
+
 urlpatterns = [
+
+    # Ajouter la vue Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += [
     path('admin/', admin.site.urls),
     path('api/actor', include('actor.urls')),  # Inclus les URLs de l'app 'myapp' sous le préfixe '/api/'
     path('api/transaction', include('transaction.urls')),  # Inclus les URLs de l'app 'myapp' sous le préfixe '/api/'
