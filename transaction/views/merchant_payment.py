64be5fa50fa35578c8737712416cecc5adad174d
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2025-03-23 22:56:19 +0100

    feat : add doc to merchant payment api

--- transaction/views/merchant_payment.py
+++ transaction/views/merchant_payment.py
@@ -8,42 +8,60 @@ from transaction.errors import PaymentProcessingError
 from transaction.serializers import MerchantPaymentSerializer
 
 from transaction.merchants.service import MerchantPaymentService
-class MerchantPaymentView(APIView):
 
+from drf_yasg.utils import swagger_auto_schema
+
+
+class MerchantPaymentView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        request_body=MerchantPaymentSerializer,  # Ajoute cette ligne
+    )
     def post(self, request, *args, **kwargs):
-
         sender = request.user
 
         # Serializer pour valider les données de la requête
         serializer = MerchantPaymentSerializer(data=request.data)
 
         if serializer.is_valid():
-            merchant_code = serializer.validated_data['merchant_code']
-            amount = serializer.validated_data['amount']
-            details = serializer.validated_data['details']
+            merchant_code = serializer.validated_data["merchant_code"]
+            amount = serializer.validated_data["amount"]
+            details = serializer.validated_data["details"]
 
             try:
                 # Appeler le service pour traiter le paiement
                 sender_wallet = Wallet.objects.get(user=sender)
                 merchant = Merchant.objects.get(merchant_code=merchant_code)
 
-                response = MerchantPaymentService.process_payment(merchant, sender_wallet,  amount, details)
+                response = MerchantPaymentService.process_payment(
+                    merchant, sender_wallet, amount, details
+                )
 
                 return Response(response, status=status.HTTP_200_OK)
 
             except ValueError as e:
                 return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
             except Merchant.DoesNotExist:
-                return Response({"detail": "Le marchand spécifié n'existe pas."}, status=status.HTTP_404_NOT_FOUND)
+                return Response(
+                    {"detail": "Le marchand spécifié n'existe pas."},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
             except Wallet.DoesNotExist:
-                return Response({"detail": "Le portefeuille de l'envoyeur n'existe pas."}, status=status.HTTP_404_NOT_FOUND)
+                return Response(
+                    {"detail": "Le portefeuille de l'envoyeur n'existe pas."},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
             except PaymentProcessingError as e:
                 # Gérer les erreurs lors du traitement du paiement
-                return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response(
+                    {"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
             except Exception as e:
                 # Gérer toute autre exception inattendue
-                return Response({"detail": "Une erreur inattendue est survenue.", "error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response(
+                    {"detail": "Une erreur inattendue est survenue.", "error": str(e)},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
