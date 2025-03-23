7ac445ffe3dcb07c9eeb5e67fb75671997cf6527
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2025-03-23 23:13:21 +0100

    feat : allow test of transaction history api with swagger ui

--- transaction/views/history.py
+++ transaction/views/history.py
@@ -1,18 +1,28 @@
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-
 from actor.models import Wallet
-from transaction.serializers import TransactionSerializer
 from transaction.models import Transaction
+from transaction.serializers import TransactionSerializer
 
 class TransactionHistoryView(generics.ListAPIView):
     serializer_class = TransactionSerializer
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'Authorization',
+                openapi.IN_HEADER,
+                description="Token JWT (format: Bearer <token>)",
+                type=openapi.TYPE_STRING,
+                required=True
+            )
+        ]
+    )
     def get_queryset(self):
-        # Récupérer l'utilisateur connecté
         user = self.request.user
         wallet = Wallet.objects.get(user=user)
-        # Récupérer toutes les transactions envoyées ou reçues par l'utilisateur
         transactions = Transaction.objects.filter(sender=wallet) | Transaction.objects.filter(receiver=wallet)
         return transactions.order_by('-timestamp')
